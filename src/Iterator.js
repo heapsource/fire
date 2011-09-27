8e7f1dcfaf5d83d0497a9cd9a4bf4b08cd53f035
Johan Hernandez <johan@firebase.co>
2011-09-27 13:50:55 -0500

    Iterator implementation and tests

---
+++ src/Iterator.js
@@ -0,0 +1,32 @@
+var Iterator = function(collection) {
+	if(!(collection instanceof Array)) {
+		throw "Iterator requires the collection to be an array but '" + collection + "' was given instead"
+	}
+	this._lastIndex= collection.length - 1;
+	this._index = -1;
+	this._collection = collection;
+}
+
+Iterator.prototype.index = function() {
+	return this._index
+}
+
+Iterator.prototype.isLast = function() {
+	return this._index === this._lastIndex
+}
+
+Iterator.prototype.current = function() {
+	return this._collection[this._index]
+}
+
+Iterator.prototype.count = function() {
+	return this._collection.length
+}
+
+Iterator.prototype.next = function() {
+	if(this.isLast()) return false;
+	this._index++;
+	return true;
+}
+
+module.exports = Iterator
---
+++ test/test.Iterator.js
@@ -0,0 +1,138 @@
+var vows = require('vows')
+var assert = require('assert')
+var Iterator = require('../src/Iterator')
+
+vows.describe('priest Iterator').addBatch({
+	"When I have an Iterator created with an empty array": {
+		topic: function() {
+			return new Iterator([])
+		},
+		"the count should be zero": function(topic) {
+			assert.equal(topic.count(), 0)
+		},
+		"isLast should return true": function(topic) {
+			assert.isTrue(topic.isLast())
+		}
+		,
+		"current should return undefined": function(topic) {
+			assert.isUndefined(topic.current())
+		},
+		"the count should be -1": function(topic) {
+			assert.equal(topic.index(), -1)
+		},
+	},
+	"When I have an Iterator created with an array of one element": {
+		topic: function() {
+			return new Iterator(["First"])
+		},
+		"the count should be 1": function(topic) {
+			assert.equal(topic.count(), 1)
+		},
+		"the index should be -1": function(topic) {
+			assert.equal(topic.index(), -1)
+		},
+		"isLast should return false": function(topic) {
+			assert.isFalse(topic.isLast())
+		},
+		"current should return undefined": function(topic) {
+			assert.isUndefined(topic.current())
+		},
+		"and when I ask for the next item": {
+			topic: function(topic) {
+				return topic.next()
+			},
+			"it should return true": function(result) {
+				assert.isTrue(result)
+			}
+		},
+		"and when I ask for the next item": {
+			topic: function(topic) {
+				topic.next()
+				return topic
+			},
+			"the current element should be the first item": function(topic) {
+				assert.equal(topic.current(), "First")
+			}
+		}
+	},
+	"When I have an Iterator created with an array of two elements": {
+		topic: function() {
+			console.warn("Creatin Iterator instance with two elemtns")
+			return new Iterator(["First","Second"])
+		},
+		"the count should be 2": function(topic) {
+			assert.equal(topic.count(), 2)
+		},
+		"isLast should return false": function(topic) {
+			assert.isFalse(topic.isLast())
+		},
+		"current should return undefined": function(topic) {
+			assert.isUndefined(topic.current())
+		},
+		"the index should be -1": function(topic) {
+			assert.equal(topic.index(), -1)
+		}
+	}
+	,
+	"When I have an Iterator created with an array of two elements": {
+		topic: function() {
+			return new Iterator(["First","Second"])
+		},
+		"and when I ask for the next item one time": {
+			topic: function(topic) {
+				assert.isTrue(topic.next())
+				return topic
+			},
+			"the index should be 0": function(topic) {
+				assert.equal(topic.index(), 0)
+			},
+			"the current element should be the first item": function(topic) {
+				assert.equal(topic.current(), "First")
+			}
+		}
+	},
+	"When I have an Iterator created with an array of two elements": {
+		topic: function() {
+			return new Iterator(["First","Second"])
+		},
+		"and when I ask for the next item two times": {
+			topic: function(topic) {
+				assert.isTrue(topic.next())
+				assert.isTrue(topic.next())
+				return topic
+			},
+			"the current element should be the last item": function(topic) {
+				assert.equal(topic.current(), "Second")
+			}
+			,
+			"isLast should return true": function(topic) {
+				assert.isTrue(topic.isLast())
+			}
+		}
+	}
+	,
+	"When I create an iterator with something is not an array": {
+		 "I should get an error": {
+			"with no arguments": function() {
+				assert.throws(function() {
+					new Iterator()
+				})
+			},
+			"with null": function() {
+				assert.throws(function() {
+					new Iterator(null)
+				})
+			},
+			"with number": function() {
+				assert.throws(function() {
+					new Iterator(2)
+				})
+			},
+			"with string": function() {
+				assert.throws(function() {
+					new Iterator("")
+				})
+			}
+		}
+	}
+	}).export(module);
